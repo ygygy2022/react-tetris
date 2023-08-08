a9206fb3f66ff261a17c91961489f430959952fd
ygygy2022 <yu.guo4@griffithuni.edu.au>
2023-08-09 08:20:03 +1000

    add functions for allow user move and drop the tetris

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -12,14 +12,60 @@ import { usePlayer } from "../hooks/usePlayer";
 import { useStage } from "../hooks/useStage";
 // Tetris component is the main component that will render the game
 const Tetris = () => {
+  // React Hooks
+  // dropTime is the time it takes for the tetromino to drop one row
   const [dropTime, setDropTime] = React.useState(null);
+  // gameOver is a boolean that will be set to true when the game is over
   const [gameOver, setGameOver] = React.useState(false);
-
-  const [player] = usePlayer();
+  // player is the tetromino that is falling down the grid
+  const [player, updatePlayerPos, resetPlayer] = usePlayer();
+  // stage is the grid that the tetrominos will be placed on
   const [stage, setStage] = useStage(player);
+  // console.log("re-render");
   console.log("re-render");
+
+  // This function will move the tetromino left or right
+  const movePlayer = (dir) => {
+    updatePlayerPos({ x: dir, y: 0 });
+  };
+
+  // This function will drop the tetromino down one row
+  const drop = () => {
+    updatePlayerPos({ x: 0, y: 1, collided: false });
+  };
+
+  // This function will drop the tetromino down one row when the down arrow key is pressed
+  const dropPlayer = () => {
+    drop();
+  };
+
+  // move is a callback function that will be called when a key is pressed
+  const move = ({ keyCode }) => {
+    if (!gameOver) {
+      // Left arrow key
+      if (keyCode === 37) {
+        movePlayer(-1);
+      }
+      // Right arrow key
+      else if (keyCode === 39) {
+        movePlayer(1);
+      }
+      // Down arrow key
+      else if (keyCode === 40) {
+        dropPlayer();
+      }
+    }
+  };
+
+  // This function will start the game
+  const startGame = () => {
+    // Reset everything
+    setStage(createStage());
+    resetPlayer();
+  };
+
   return (
-    <StyledTetrisWrapper>
+    <StyledTetrisWrapper role="button" tabIndex="0" onKeyDown={(e) => move(e)}>
       <StyledTetris>
         <Stage stage={stage} />
         <aside>
@@ -33,7 +79,7 @@ const Tetris = () => {
               <Display text="Level" />
             </div>
           )}
-          <StartButton />
+          <StartButton callback={startGame} />
         </aside>
       </StyledTetris>
     </StyledTetrisWrapper>
--- src/hooks/usePlayer.js
+++ src/hooks/usePlayer.js
@@ -1,16 +1,35 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 
-import { randomTetromino } from "../tetrominos";
+import { randomTetromino, TETROMINOS } from "../tetrominos";
+import { STAGE_WIDTH } from "../gameHelpers";
 // The player is the tetromino that is falling down the grid
 export const usePlayer = () => {
   const [player, setPlayer] = useState({
     // The tetromino position
     pos: { x: 0, y: 0 },
     // The tetromino shape
-    tetromino: randomTetromino().shape,
+    tetromino: TETROMINOS[0].shape,
     // The tetromino collided
     collided: false,
   });
 
-  return [player];
+  // This function will update the player position
+  const updatePlayerPos = ({ x, y, collided }) => {
+    setPlayer((prev) => ({
+      ...prev,
+      pos: { x: (prev.pos.x += x), y: (prev.pos.y += y) },
+      collided,
+    }));
+  };
+
+  // This function will reset the player
+  const resetPlayer = useCallback(() => {
+    setPlayer({
+      pos: { x: STAGE_WIDTH / 2 - 2, y: 0 },
+      tetromino: randomTetromino().shape,
+      collided: false,
+    });
+  }, []);
+
+  return [player, updatePlayerPos, resetPlayer];
 };
